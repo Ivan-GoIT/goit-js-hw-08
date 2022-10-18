39804a0f6a475ff2cfbba66064a3260ea7ec84a2
IvanKoshel98 <GitHubPassvord142>
2022-10-18 16:04:53 +0300

    work with  problemm object in storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const checkLocalstorage = key => {
+  return !!localStorage.getItem(key);
+};
+
+const onClick = () => {
+  console.log(Object.values(localStorage.getItem('feedback-form-state')));
+};
+
+const onKeyDown = evt => {
+  const key = 'feedback-form-state';
+
+  const objFromStorage = checkLocalstorage(key)
+    ? { ...localStorage.getItem(key) }
+    : {};
+
+    localStorage.setItem(key, {...localStorage.getItem(key), [`${evt.target.name}`] : evt.target.value});
+  // objFromStorage[evt.target.name] = evt.target.value;+
+    //localStorage.clear()
+};
+
+formEl.addEventListener('input', onKeyDown);
+formEl.addEventListener('click', onClick);
